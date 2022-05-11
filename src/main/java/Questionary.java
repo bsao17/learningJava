be112abbba92484ca8581e0f99b5afee7d20beb2
bsao17 <declic62@gmail.com>
2022-05-11 08:08:27 +0200

    Clean code

--- src/main/java/Questionary.java
+++ src/main/java/Questionary.java
@@ -1,18 +1,20 @@
 public class Questionary {
     public static final String title = "welcome";
+
     public static void main(String[] args) {
         System.out.println(title);
-        sayHelloTo();
+        sayHelloTo("hello");
         System.out.println(monAge(new int[]{17, 48, 70, 72}));
     }
-    private static void sayHelloTo(){
-        System.out.println("hello " + "world");
+
+    private static void sayHelloTo(String recipe) {
+        System.out.println(recipe + "-" + "world");
     }
 
-    private static int monAge (int[] ages){
+    private static int monAge(int[] ages) {
         int acc = 0;
         int overall = 0;
-        for (int age : ages){
+        for (int age : ages) {
             acc = acc + age;
         }
         overall = acc / ages.length;
