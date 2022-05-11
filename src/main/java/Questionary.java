adddf4efccc35cf5ea3e2bc5cfd6e7d7a5f318c2
bsao17 <declic62@gmail.com>
2022-05-11 08:02:48 +0200

    ages overall

--- src/main/java/Questionary.java
+++ src/main/java/Questionary.java
@@ -1,11 +1,21 @@
 public class Questionary {
-    public static String title = "welcome";
+    public static final String title = "welcome";
     public static void main(String[] args) {
         System.out.println(title);
-        sayHelloTo("world");
+        sayHelloTo();
+        System.out.println(monAge(new int[]{17, 48, 70, 72}));
+    }
+    private static void sayHelloTo(){
+        System.out.println("hello " + "world");
     }
 
-    private static void sayHelloTo(String recipe){
-        System.out.println("hello " + recipe);
+    private static int monAge (int[] ages){
+        int acc = 0;
+        int overall = 0;
+        for (int age : ages){
+            acc = acc + age;
+        }
+        overall = acc / ages.length;
+        return overall;
     }
 }
